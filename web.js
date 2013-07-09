7a88f347488f636d102e82219cf74db78c61025d
Ubuntu <ubuntu@ip-172-31-41-98.us-west-2.compute.internal>
2013-07-09 01:19:30 +0000

    reading from disk index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ 
+  var fs = require('fs');
+  var buffer = fs.readFileSync('index.html');
+  var str = buffer.toString();
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
